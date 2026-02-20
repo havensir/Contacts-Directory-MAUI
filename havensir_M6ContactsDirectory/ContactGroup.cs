f6069ed1b159c731e82349d6fcd82e8c49890a72
ocahavens <ocahavens@gmail.com>
2026-02-20 10:23:29 -0500

    Add Contact and ContactGroup classes; update project file
    Added Contact and ContactGroup classes for contact management and grouping. Fixed app icon path in .csproj, ensured ContactDetailsPage.xaml is compiled, and removed dotnet_bot.png resize entry.

---
+++ havensir_M6ContactsDirectory/ContactGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace havensir_M6ContactsDirectory
+{
+    public class ContactGroup : ObservableCollection<Contact>
+    {
+        public string Title { get; set; }
+
+        public ContactGroup(string title, IEnumerable<Contact> contacts) : base(contacts)
+        {
+            Title = title;
+        }
+    }
+}
